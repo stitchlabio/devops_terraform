dccd7db680c59245bb9fb2ce6756ad6fec2303ab
kc <kcfigaro@gmail.com>
2019-11-17 15:18:30 +0000

    update region to ap-northeast-1

--- step0/main.tf
+++ step0/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "us-west-2"
+    region = "ap-northeast-1"
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0a85857bfc5345c38"
+    ami = "ami-0f9af249e7fa6f61b" #ubuntu1604
     instance_type = "t2.micro"
     
     tags = {
